85c04cea78a25604b02db1f8624be07335520f7a
jossyahia <jossyahia@gmail.com>
2024-05-29 15:17:30 +0100

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -12,18 +12,17 @@ export default function Home() {
       <section className="space-y-6 pb-8 pt-6 md:pb-12 md:mt-10 lg:py-32">
         <div className="container flex flex-col gap-4 text-center">
           <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance">
-            Hello, I&apos;m James
+            Hello, Welcome To Free Positives
           </h1>
           <p className="max-w-[42rem] mx-auto text-muted-foreground sm:text-xl text-balance">
-            Welcome to my blog template. Built using tailwind, shadcn, velite
-            and Nextjs 14.
+            "Your Daily Dose of Positive Energy and Inspiration"
           </p>
           <div className="flex flex-col gap-4 justify-center sm:flex-row">
             <Link
               href="/blog"
               className={cn(buttonVariants({ size: "lg" }), "w-full sm:w-fit")}
             >
-              View my blog
+             Get Some Positive Vibes
             </Link>
             <Link
               href={siteConfig.links.github}
