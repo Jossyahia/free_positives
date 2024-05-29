395a4171136839f77421f6755b3eda41bfedfb67
jossyahia <jossyahia@gmail.com>
2024-05-29 13:03:07 +0100

    first commit

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "Free Positives Blog",
+  url: "https://example.com",
+  description: "Free positives For Positive Vibes Only No Negative Energy Only Positive Energy ",
+  author: "Fastfast Logistics Services",
+  links: {
+    twitter: "/",
+    github: "/ ",
+    personalSite: "https://fastfast.ng",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
